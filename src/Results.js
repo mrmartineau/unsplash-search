d93678ad2135e39648abc883b6cd6f613061c43d
Zander Martineau <zander@martineau.tv>
2017-05-28 23:04:35 +0100

    improve results component

--- src/Results.js
+++ src/Results.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import './Results.css';
 import Image from './Image';
+import Message from './Message';
 
-export default (props) => {
-	const {results} = props;
-	// console.log('results', results);
-	const images = results.map(result => (
-		<Image src={result.urls} key={result.id} href={result.links.html} username={result.user.username} color={result.color} download={result.links.download} />
-	));
-	return (
-		<div className="Results">
-			{images}
-		</div>
-	)
-}
+export default props => {
+  const { results } = props;
+  console.log('results', results);
+  const images = results.map(result => (
+    <Image
+      src={result.urls}
+      key={result.id}
+      href={result.links.html}
+      username={result.user.username}
+      color={result.color}
+      download={result.links.download}
+    />
+  ));
+  return (
+    <div className="Results">
+      {
+        props.results.length > 0
+        ?
+          images
+        : <Message>No results 😢</Message>
+      }
+    </div>
+  );
+};
